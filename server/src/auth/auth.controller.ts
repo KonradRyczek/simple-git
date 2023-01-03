74f301e1779ad4f17a6b20db751fb6889f879a09
Konrad <konrad.ryczek@student.up.krakow.pl>
2023-01-03 23:38:58 +0100

    Rewrote signup method in auth.controller, not yet implemented on the service side

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,8 +10,16 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup(@Body() dto: AuthDto) {
-        return this.authService.signup(dto);
+    @UseInterceptors(FileInterceptor('pubKey'))
+    async signup(@Body() body: any, @UploadedFile() file) {
+        const fileContents = await file.readFile();
+        const fileAsString = fileContents.toString();
+        const dto : SignUpDto = new SignUpDto();
+        dto.username = body.username;
+        dto.email = body.email;
+        dto.password = body.password;
+        dto.sshPublicKey = body.pubKey;
+        // return this.authService.signup(dto);
     }
 
     @Post("signin")
---
+++ server/src/auth/dto/signup.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+
+
+export class SignUpDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+
+    @IsString()
+    @IsNotEmpty()
+    username: string
+
+    @IsString()
+    @IsNotEmpty()
+    password: string
+
+    @IsString()
+    @IsNotEmpty()
+    sshPublicKey: string
+}
